5d35a3a73e84c8c43915908227db2bb06e506704
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-06-18 14:44:47 +0700

    update

--- src/tutorial/3-conditional-rendering/mysolution/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/mysolution/3-show-hide.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import UseEffectCleanup from "../../2-useEffect/mysolve/2-useEffect-cleanup";
 
 const ShowHide = () => {
