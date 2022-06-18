2ee48f9bda831d50dee30c80dab38a7d43f16bf4
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2022-06-18 14:02:30 +0700

    update components

--- src/components/RenderTutorial.js
+++ src/components/RenderTutorial.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const RenderTutorial = (props) => {
+  if (props.children.length > 1) {
     return (
-        <div>
-            {props.children.map((child, index) => {
-                return (
-                    <div key={index}>
-                        {child}
-                        <br />
-                        <br />
-                        <hr />
-                    </div>
-                );
-            })}
-        </div>
+      <div>
+        {props.children.map((child, index) => {
+          return (
+            <div key={index}>
+              {child}
+              <br />
+              <br />
+              <hr />
+            </div>
+          );
+        })}
+      </div>
     );
+  } else {
+    return <div>{props.children}</div>;
+  }
 };
 
 export default RenderTutorial;
